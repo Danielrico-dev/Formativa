c17cad9c15537363388900c5f92a59baeaf536c7
Danielrico-dev <ricodaniel147@gmail.com>
2026-03-16 17:13:24 -0300

    Escolha-operacao
    Permite agora escolher a operação

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ print("Calculadora em Python")
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-resultado = num1 + num2
-
-print("Resultado:", resultado)
+operacao = input("Escolha a operação (+ - * /): ")
